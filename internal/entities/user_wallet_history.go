d3670a39d14057254d8f090e3c44492031f68ca3
1Suro1 <timu.zaxarov@gmail.com>
2025-02-27 18:45:13 +0900

    fix

--- internal/entities/user_wallet_history.go
+++ internal/entities/user_wallet_history.go
@@ -7,7 +7,6 @@ type (
 	}
 
 	UserWalletHistory struct {
-		ID          uint    `gorm:"column:id;primaryKey;autoIncrement"`
 		UserID      int     `gorm:"column:user_id"`
 		Coin        float64 `gorm:"column:coin;type:numeric(10,2)"`
 		RefillType  string  `gorm:"column:refill_type;type:varchar(255)"`
